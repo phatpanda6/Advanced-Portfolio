8562dba91781115e262cc039c862e6bd62bc3c99
Alan <ahuang24365@gmail.com>
2024-09-18 14:33:29 +1000

    first FINAL draft, need to make resume buttons work

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
-// template_di02so4
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1/20; 
 
-// service_vcwowx7
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
 
-//public id: 4HTtBiU3Td6v4lSIQ
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd =  i % 2 !==  0;
+        const boolInt = isOdd? -1 :1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
+
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_vcwowx7",
+      "template_di02so4",
+      event.target,
+      "4HTtBiU3Td6v4lSIQ"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "the email service is temporarily unavaiable. Please contact me directly on ahuang24365@gmail.com"
+      );
+    });
+}
+
+function toggleModal() {
+  //toggle modal
+
+  if (isModalOpen) {
+    isModalOpen = false;
+
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
